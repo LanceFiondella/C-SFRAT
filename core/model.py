0a556c7b49160bbf51a328f22c08f82e6cd036fc
KenanChen3 <43079108+KenanChen3@users.noreply.github.com>
2020-01-14 10:27:15 -0500

    Changed intial estimates values

--- core/model.py
+++ core/model.py
@@ -86,7 +86,8 @@ class Model(ABC):
         pass
 
     def initialEstimates(self, min = 0.0, max = 0.01):
-        return np.insert(np.random.uniform(min, max, self.numCovariates),0, np.random.uniform(0.75999, 0.99999,1))
+        #return np.insert(np.random.uniform(min, max, self.numCovariates), 0, np.random.uniform(0.0, 0.1, 1)) #Works for GM and NB2
+        return np.insert(np.random.uniform(min, max, self.numCovariates),0, np.random.uniform(0.998, 0.99999,1))
                                                                     # (low, high, size)
                                                                     # size is numCovariates + 1 to have initial estimate for b
 
